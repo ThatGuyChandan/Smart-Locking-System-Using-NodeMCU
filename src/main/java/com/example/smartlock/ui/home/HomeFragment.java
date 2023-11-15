2feb460d111bfb6b32a702588d4a21baf490ba3d
vldot <lokesh.21bce8017@vitapstudent.ac.in>
2023-11-16 01:28:06 +0530

    Initial Commit

---
+++ src/main/java/com/example/smartlock/ui/home/HomeFragment.java
@@ -0,0 +1,76 @@
+package com.example.smartlock.ui.home;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.smartlock.databinding.FragmentHomeBinding;
+
+import java.net.ProtocolException;
+
+public class HomeFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        HomeViewModel homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+
+        final WebView homeWebView = binding.idWebViewHome;
+        final ProgressBar loadingPB = binding.idPBLoading;
+        homeWebView.loadUrl("http://192.168.1.123");
+        homeWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                loadingPB.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                loadingPB.setVisibility(View.GONE);
+            }
+        });
+        homeWebView.getSettings().setJavaScriptEnabled(true);
+        homeWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+
+        homeWebView.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if(event.getAction() == event.ACTION_DOWN){
+                    switch (keyCode){
+                        case KeyEvent.KEYCODE_BACK:
+                            if(homeWebView.canGoBack()){
+                                homeWebView.goBack();
+                            }
+                    }
+                }
+                return false;
+            }
+        });
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
---
+++ src/main/java/com/example/smartlock/ui/slideshow/SlideshowFragment.java
@@ -0,0 +1,76 @@
+package com.example.smartlock.ui.slideshow;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.service.voice.VisibleActivityInfo;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.smartlock.databinding.FragmentSlideshowBinding;
+
+public class SlideshowFragment extends Fragment {
+
+    private FragmentSlideshowBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        SlideshowViewModel slideshowViewModel =
+                new ViewModelProvider(this).get(SlideshowViewModel.class);
+
+        binding = FragmentSlideshowBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final WebView creditsView = binding.idWebViewCredits;
+        final ProgressBar loadingPB = binding.idPBLoading;
+        creditsView.loadUrl("");
+        creditsView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                loadingPB.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                loadingPB.setVisibility(View.GONE);
+            }
+        });
+
+        creditsView.getSettings().setJavaScriptEnabled(true);
+        creditsView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+
+
+        creditsView.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if(event.getAction() == KeyEvent.ACTION_DOWN){
+                    switch (keyCode){
+                        case KeyEvent.KEYCODE_BACK:
+                            if(creditsView.canGoBack()){
+                                creditsView.goBack();
+                            }
+                    }
+                }
+                return false;
+            }
+        });
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
